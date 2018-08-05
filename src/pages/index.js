9defe2efe2e86fef3d386c519d5fbc2e73e7ac9b
Rohit Jindal <rohit.jindal@PW-C02RK2ASG8WN.local>
2018-08-05 15:37:20 +0530

    Added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './index.css';
 import myPhoto from "../../static/my.jpg";
-import '../../static/Roboto-Light.ttf';
+import ln from "../../static/linkedin.svg";
+import fb from "../../static/fb.svg";
+import gh from "../../static/github.svg";
 
 const IndexPage = () => (
   <div className="mainDiv">
@@ -14,8 +16,18 @@ const IndexPage = () => (
       <div className="introDiv">
         <div style={{ 'font-size': '30px' }}>Hi, I'm Rohit</div>
         <br />
-        <div>I develop and code User Interfaces & I love what I do</div>
+        <div style={{ 'font-size': '24px' }}>I'm a Frontend developer at <span className="company">Flipkart</span>, India</div>
+        <br />
+        <div style={{ 'font-size': '24px' }}>I love developing User Interfaces</div>
+        <br />
+        <div style={{ 'font-size': '24px' }}>Get in touch</div>
+        <br />
       </div>
+    </div>
+    <div className="appsdiv">
+      <a target="_blank" href="https://www.linkedin.com/in/jindalrohit18"><img className="apps" src={ln} /></a>
+      <a target="_blank" href="https://www.facebook.com/jindal.rohit018"><img className="apps" src={fb} /></a>
+      <a target="_blank" href="https://github.com/rohitjindal18"><img className="apps" src={gh} /></a>
     </div>
   </div>
 )
